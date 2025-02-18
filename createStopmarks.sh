633d6f531233eb9b8a52c29cb32c5ffa7e8a0970
Jonathan Vega <jon.e.vega@icloud.com>
2025-02-19 03:31:04 +0900

    made variables into UPPER CASE to keep script neat

--- createStopmarks.sh
+++ createStopmarks.sh
@@ -2,25 +2,26 @@
 
 # this script creates txt files with the runtime (in seconds) for each video in ~/piTVstation/videos
 
-sumFilesCreated=0
+SUM_FILES_CREATED=0
+LIVE_M3U8_SECONDS_DURATION=30
 
 for dir in /home/$USER/piTVstation/videos/*; do
 	if [[ ! -f "${dir%.*}.txt" && "${dir,,}" == *live* ]]; then
 		echo "creating live: ${dir%.*}.txt"
 		touch "${dir%.*}.txt"
-		echo "86400" > "${dir%.*}.txt" #86400 seconds are in a day
-		sumFilesCreated=$((sumFilesCreated+1))
+		echo "$LIVE_M3U8_SECONDS_DURATION" > "${dir%.*}.txt" #86400 seconds are in a day
+		SUM_FILES_CREATED=$((SUM_FILES_CREATED+1))
 	elif [ ! -f "${dir%.*}.txt" ]; then
 		duration=$(mediainfo --Inform="Video;%Duration%" "$dir")
 		if [ -z "$duration" ]; then  # Check if mediainfo duration is empty
 			echo "skipping: $dir"
-        else
+		else
         	echo "creating file: ${dir%.*}.txt"
         	touch "${dir%.*}.txt"
         	echo $(( ${duration%.*} / 1000 )) > "${dir%.*}.txt"  # Convert ms to seconds
-        	sumFilesCreated=$((sumFilesCreated+1))
+        	SUM_FILES_CREATED=$((SUM_FILES_CREATED+1))
         fi
 	fi
 done
 
-echo "$sumFilesCreated .txt files created"
+echo "$SUM_FILES_CREATED .txt files created"
